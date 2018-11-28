5d90088a1dcab94ccfbc5d1ac1c4097e7b479b01
sandishui <san_dishui@163.com>
2018-11-28 18:56:39 +0800

    Update CheckTwo.py

--- CheckProtocol/CheckTwo.py
+++ CheckProtocol/CheckTwo.py
@@ -5,10 +5,10 @@ def getHttpCode(urlpath):
     try:
         response = urllib2.urlopen(urlpath)
     except urllib2.HTTPError as e:
-        #if(e.code == 400):
-        #    return  400
-        if(e.code == 404):
-            return  404
+        if(e.code == 400):
+            return  400
+        #if(e.code == 404):
+        #    return  404
         raise RuntimeError('HTTPError')
     except urllib2.URLError:
         raise RuntimeError('URLError')
